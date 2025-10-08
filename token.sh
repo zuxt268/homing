8e90b9301d462b1096e8e30e851e23a7ccf01968
zuxt268 <y.ikezawa93@gmail.com>
2025-10-08 23:39:00 +0900

    mod sh

--- token.sh
+++ token.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] token" >> /var/www/homing/batch.log
-curl -X POST http://homing_app_1/api/token/check >> /var/www/homing/batch.log 2>&1
+curl -X POST http://127.0.0.1:8090/api/token/check >> /var/www/homing/batch.log 2>&1
