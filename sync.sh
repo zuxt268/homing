662ae5883fc5818ec4a5ea5a38dc66cd4e6c3373
zuxt268 <y.ikezawa93@gmail.com>
2025-11-24 23:23:01 +0900

    mod

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 echo "[$(date '+%Y-%m-%d %H:%M:%S')]　sync" >> /var/www/homing/batch.log
-curl -X POST http://127.0.0.1:8090/api/sync >> /var/www/homing/batch.log 2>&1
+curl -X POST http://localhost:8090/api/sync >> /var/www/homing/batch.log 2>&1
