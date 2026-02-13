b5c778d3257897acdd10c9cb554e40019b64d40a
zuxt268 <y.ikezawa93@gmail.com>
2026-02-13 12:58:01 +0900

    mod

---
+++ script/fetch-google.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] token" >> /var/www/homing/batch.log
+curl -X POST http://localhost:8090/api/google-business/fetch >> /var/www/homing/batch.log 2>&1
