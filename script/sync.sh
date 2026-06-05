969af39fbaf20e22225e619e0ecff4625f1dfca9
zuxt268 <y.ikezawa93@gmail.com>
2026-06-05 17:36:41 +0900

    mod

--- script/sync.sh
+++ script/sync.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')]　sync: instagram -> wordpress" >> /var/www/homing/batch.log
-curl -X POST http://localhost:8090/api/sync/wordpress-instagram >> /var/www/homing/batch.log 2>&1
 
-echo "[$(date '+%Y-%m-%d %H:%M:%S')]　sync: instagram -> GBP" >> /var/www/homing/batch.log
-curl -X POST http://localhost:8090/api/sync/business-instagram >> /var/www/homing/batch.log 2>&1
+curl -X POST http://localhost:8090/api/sync/wordpress-instagram
+
+curl -X POST http://localhost:8090/api/sync/business-instagram
+
+curl -X POST http://localhost:8090/api/sync/wordpress-gbp
